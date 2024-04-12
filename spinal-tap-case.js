f553a66eb3d52ee38cf948ca005901a00ced8768
Gundalf21 <mihaiandreineagoe@gmail.com>
2024-04-12 15:57:26 +0300

    added a couple of the exercises

---
+++ spinal-tap-case.js
@@ -0,0 +1,9 @@
+function spinalCase(str) {
+  let reg = /\s+|_+/g;
+
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  return str.replace(reg, "-").toLowerCase();
+}
+
+spinalCase("This Is Spinal Tap");
